5060b46a1fc07b21f9cf905e9278bf2c7aab24c9
Your Name <khalidsaifullaha3@gmail.com>
2024-12-29 19:58:17 +0600

    dessert menu added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,13 +1,14 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImage from "../../../assets/menu/banner3.jpg";
+import dessertImage from "../../../assets/menu/dessert-bg.jpeg";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import useMenu from "../../../Hooks/useMenu";
 import MenuCategory from "../MenuCategory.jsx/MenuCategory";
 
 const Menu = () => {
   const [menu] = useMenu();
-  const dessert = menu.filter((item) => item.category === "dessert");
+  const desserts = menu.filter((item) => item.category === "dessert");
   const soup = menu.filter((item) => item.category === "soup");
   const salad = menu.filter((item) => item.category === "salad");
   const drinks = menu.filter((item) => item.category === "drinks");
@@ -19,11 +20,19 @@ const Menu = () => {
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuImage} title="Our menu"></Cover>
+      {/* main cover */}
       <SectionTitle
         subHeading="Don't Miss"
         heading="Today's Offer"
       ></SectionTitle>
+      {/* offered menu items */}
       <MenuCategory items={offered}></MenuCategory>
+      {/* dessert menu items */}
+      <MenuCategory
+        items={desserts}
+        title="Desert"
+        img={dessertImage}
+      ></MenuCategory>
     </div>
   );
 };
