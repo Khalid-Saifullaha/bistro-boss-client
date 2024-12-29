e06235568f64cddbb1f11349cd5bece68d21b178
Your Name <khalidsaifullaha3@gmail.com>
2024-12-29 19:43:40 +0600

    create coustom hook and parallax added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,11 +1,27 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImage from "../../../assets/menu/banner3.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import useMenu from "../../../Hooks/useMenu";
+
 const Menu = () => {
+  const [menu] = useMenu();
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const drinks = menu.filter((item) => item.category === "drinks");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <h2>Menu</h2>
+      <Cover img={menuImage} title="Our menu"></Cover>
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
     </div>
   );
 };
