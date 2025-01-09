137aa5d5c5a58da6a978721fb2ae1d772054b45a
Your Name <khalidsaifullaha3@gmail.com>
2025-01-09 22:40:00 +0600

    load data and display stats on the admin home

--- src/pages/DashBoard/AdminHome/AdminHome.jsx
+++ src/pages/DashBoard/AdminHome/AdminHome.jsx
@@ -1,14 +1,67 @@
 import React from "react";
 import useAuth from "../../../Hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaBorderAll, FaUsers } from "react-icons/fa";
+import { IoWallet } from "react-icons/io5";
+import { CiMenuBurger } from "react-icons/ci";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats, isLoading } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
+  if (isLoading) {
+    return "Loading";
+  }
   return (
     <div>
       <h2 className="text-3xl">
         <span>Hi, Welcome</span>
         {user?.displayName ? user.displayName : "Back"}
       </h2>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <IoWallet></IoWallet>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">{stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaUsers></FaUsers>
+          </div>
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <CiMenuBurger></CiMenuBurger>
+          </div>
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stats.menuItems}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaBorderAll></FaBorderAll>
+          </div>
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stats.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
